df28bc4691cc0901e610d562a9b6a28e7d9bd631
hirokihida-dot <cramale@upv.edu.es>
2026-04-20 13:01:36 +0200

    Delete Práctica 2 directory

--- Práctica 2/include/measurableAlgorithms.h
+++
@@ -1,5 +0,0 @@
-int linearSearch(int a[], int tamanyo, int e);
-void selectionSort(int a[], int tamanyo);
-void insertionSort(int a[], int tamanyo) ;
-void mergeSort(int a[], int left, int right) ;
-void naturalMerge(int a[], int left, int half, int right) ;
--- Práctica 2/plots/SortingSearchPlot.gp
+++
@@ -1,9 +0,0 @@
-set xrange [0:10000] 
-set yrange [0:]
-set xtics 200
-set ytics 6
-set xlabel "Size"
-set ylabel "Microseconds"
-set key left
-set grid
-plot "plots/SortingSearch.out" using 1:2 title "Average case" with points
